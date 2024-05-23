60b91db13ecb6a2898403e97e3e056ddee46eab1
corey <corey.cushnie@gmail.com>
2024-05-23 11:14:04 -0400

    add placeholder response data

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const PORT = 6446;
 const app = express();
 
 app.get("/", (req, res) => {
+  return res.json({ success: true });
+});
+
+app.get("/users", async (req, res) => {
   try {
-    return res.json({ success: true });
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    if (!response.ok) throw Error("Response error");
+
+    return res.json(data);
   } catch (e) {
-    return res.json({ success: false, message: e.message });
+    return res.json({ message: e.message });
   }
 });
 
